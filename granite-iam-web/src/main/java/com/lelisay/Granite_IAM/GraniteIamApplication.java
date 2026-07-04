26c1b4859161422d42e5b3ba278e031f52db9450
Lelisayohanes <134288780+Lelisayohanes@users.noreply.github.com>
2026-07-04 21:42:39 +0300

    initial, commit

---
+++ granite-iam-web/src/main/java/com/lelisay/Granite_IAM/GraniteIamApplication.java
@@ -0,0 +1,13 @@
+package com.lelisay.Granite_IAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraniteIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraniteIamApplication.class, args);
+	}
+
+}
---
+++ granite-iam-web/src/test/java/com/lelisay/Granite_IAM/GraniteIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lelisay.Granite_IAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraniteIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
